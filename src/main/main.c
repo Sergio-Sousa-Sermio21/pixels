db8e02ca460ed71d0121bf961e01e72745510078
Joao Cardoso <zerozerologic@gmail.com>
2023-07-30 20:53:51 +0100

    OpenGLallthewaybaby

--- src/main/main.c
+++ src/main/main.c
@@ -1,11 +1,26 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include <gl/glaux.h>
+#include <stdio.h> // Defines scanf() and printf().
+#include <stdlib.h> // Defines atoi(), atof(), rand().
+#include <math.h> // Defines math functions such as exp(x,y).
+#include <time.h> // Defines time.
+#include <string.h> // obv
+#include <assert.h> // obv
+#include <errno.h> // Defines some explicit errors to aid in debugging.
+#include 
 
 
+bool full_screen = false;
 
 
 
 int main() {
-    printf("Hello World!");
+    int seed = time(NULL);
+    srand(seed);
+
+
+
     return 0;
 }
